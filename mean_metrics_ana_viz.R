68f83524002d17c352f360db129ad82bd0e0cd7c
MaxKorbmacher <64847184+MaxKorbmacher@users.noreply.github.com>
2023-07-07 12:25:35 +0200

    small plotting updates

--- mean_metrics_ana_viz.R
+++ mean_metrics_ana_viz.R
@@ -17,9 +17,13 @@ library(magrittr)
 #install.packages("olsrr")
 library(olsrr)
 library(ggeffects)
-library(nlme)
+library(mgcv)
 #install.packages("ggpmisc")
 library(ggpmisc)
+library(lmerTest)
+#install.packages("sjPlot")
+library(sjPlot)
+
 # set working directory where the data files are situated
 setwd("/home/max/Documents/Projects/Diffusion/UKBlong/data_export/")
 # load data (without DRAD extra outliers removed as there were many outliers)
@@ -70,32 +74,28 @@ df_orig = data
 # }
 # >> most metrics are homeoscedastic in the model that we plan to use. 
 # make list for all the models (linear and non-linear)
-# linear_models = list()
-# #non_linear_models = list()
-# for (i in outcome_vars){
-#   f = formula(paste(i,"~ age*sex + TP + (1|site_t3)+(1|eid)"))
-#   linear_models[[i]] = lmer(f, data=data)
-#   #non_linear_models[[i]] = nlmer(f, data=data)
-# }
-# 
-# # now, we predict from those models and make a list (predictions)containing the predictions for each metric 
-# linear_predictions = list()
-# non_linear_predictions = list()
-# age = data$age
-# ID = data$eid
-# TP = data$TP
-# data1 = data.frame(ID, age, TP)
-# data2 = data1
-# for (i in 1:length(outcome_vars)){
-#   data1$fit = predict(linear_models[[i]])
-#   #data2$fit = predict(non_linear_models[[i]])
-#   linear_predictions[[i]] = data1
-#   #non_linear_predictions[[i]] = data2
-# }
+linear_models = list()
+#non_linear_models = list()
+for (i in outcome_vars){
+  f = formula(paste(i,"~ age*sex + TP + (1|site_t3/eid)"))
+  linear_models[[i]] = lmer(f, data=data)
+  #non_linear_models[[i]] = nlmer(f, data=data)
+}
+ 
+# # now, we predict from those models and make a list (predictions)containing the predictions for each metric
+linear_predictions = list()
+age = data$age
+ID = data$eid
+TP = data$TP
+data1 = data.frame(ID, age, TP)
+data2 = data1
+for (i in 1:length(outcome_vars)){
+  data1$fit = predict(linear_models[[i]])
+  linear_predictions[[i]] = data1
+}
 
 # give the prediction list the right names
 names(linear_predictions) = outcome_vars
-#names(non_linear_predictions) = outcome_vars
 
 # make a list of names for the y-Axis
 y_lab = c("BRIA - V intra", "BRIA - V extra", "BRIA - V csf", "BRIA - micro RD","BRIA - micro FA",
@@ -107,34 +107,30 @@ y_lab = c("BRIA - V intra", "BRIA - V extra", "BRIA - V csf", "BRIA - micro RD",
           "WMTI - axEAD", "WMTI - AWF", "WMTI - radEAD")
 
 ######## PLOTTING ####
-# first run gams and lms for each time point to get R squared values
+# first run gams and lms on the fitted values to get R squared values for the plots
 lm_r.sq1 = c()
-lm_r.sq2 = c()
 gam_r.sq1 = c()
-gam_r.sq2 = c()
 
 for (i in 1:27){
-  T1_lm_model = lm(T1[[i]]~(age)+age:sex+sex, data = T1)
+  T1_lm_model = lm(fit~(age), data = linear_predictions[[i]])
   lm_r.sq1[i] = summary(T1_lm_model)$r.sq
-  T2_lm_model = lm(T2[[i]]~(age)+age:sex+sex, data = T2)
-  lm_r.sq2[i] = summary(T2_lm_model)$r.sq
-  T1_gam_model = gam(T1[[i]]~s(age)+age:sex+sex, data = T1)
+  T1_gam_model = gam(fit~s(age), data = linear_predictions[[i]])
   gam_r.sq1[i] = summary(T1_gam_model)$r.sq
-  T2_gam_model = gam(T2[[i]]~s(age)+age:sex+sex, data = T2)
-  gam_r.sq2[i] = summary(T2_gam_model)$r.sq
 }
-# #for an overview of the values:
-# data.frame(lm_r.sq1,lm_r.sq2,gam_r.sq1,gam_r.sq2)
+#for an overview of the values:
+data.frame(lm_r.sq1,gam_r.sq1)
 
-# SCATTER PLOT OF PREDICTED VALUES >> DIFFERENTIATING BETWEEN TIME POINTS ####
+# SCATTER PLOT OF PREDICTED VALUES ####
 # now, plot each of the predictions in a scatter plot with two time points and a fitted line
 lm_plot = list()
 for (i in 1:length(y_lab)){
-  lm_plot[[i]] = ggplot(data=linear_predictions[[i]],aes(x=age, y=fit,color = TP))+ 
+  lm_plot[[i]] = ggplot(data=linear_predictions[[i]],aes(x=age, y=fit))+ 
   #geom_line(aes(y=fit,group=ID),alpha=.4,lwd=.5) + #,col=TP
   #geom_point(aes(y=fit,shape=factor(TP), col=TP),alpha=.6) +#
   #geom_ribbon(aes(group=TP,ymin=lwr,ymax=upr),alpha=0.3,fill="grey") + 
-  geom_smooth(method = "lm") +
+  geom_smooth(method = "lm", color= "#E69F00") +
+  geom_smooth(method = "gam", color = "#56B4E9") +
+  geom_line(aes(y=0),linetype="dotted")+
     # stat_regline_equation(
     #   aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~")),
     #   formula = formula(paste("data"[i]"~age")), data =data) +
@@ -146,68 +142,89 @@ for (i in 1:length(y_lab)){
   theme_bw() + 
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank())+
-  theme(legend.position = "none")+
-    labs(title = paste("TP1 R2 = ",round(lm_r.sq1[i],digits = 4), "      ","TP2 R2 = ", round(lm_r.sq2[i],digits = 4)))
+  theme(legend.position = "none") +
+  labs(title = (paste("linear R2 = ",round(lm_r.sq1[i],digits = 4), "      ","non-linear R2 = ", round(gam_r.sq1[i],digits = 4))))
+}
+# 
+# annoted = c()
+# for (i in 1:length(y_lab)){
+#   #(noquote(paste("'TP1 R'*^2*' = ",round(gam_r.sq1[i],digits = 4), "~~~~~~~TP2 'R^2' = ", round(gam_r.sq2[i],digits = 4),"'")))
+#   lm_plot[[i]] = annotate_figure(lm_plot[[i]], top = bquote(R^2==.(round(gam_r.sq1[i],digits = 4))))
+# }
+# lm_plot[[2]]
+# bquote(R^2==.(round(gam_r.sq1[i],digits = 4)))
+
+#plot = ggarrange(plotlist = lm_plot, ncol = 3)
+plot= do.call("grid.arrange", c(lm_plot,ncol = 3))
+ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/ADJ_ageing_plots.pdf",plot, width = 15, height = 20)
+
+
+
+
+# SCATTER PLOT DIFFERENTIATING BETWEEN TIME POINTS (Cross-Sectional plotting)
+# first do the cross-sectional predictions
+lm_r.sq1 = c()
+lm_r.sq2 = c()
+gam_r.sq1 = c()
+gam_r.sq2 = c()
+tmp_data = data %>% dplyr::select(eid, age)
+tmp_data$model = data$TP
+levels(tmp_data$model) = c("linear", "non-linear")
+data_TP1 = list()
+data_TP2 = list()
+for (i in 1:27){
+  T1_lm_model = lm(T1[[i]]~(age)+age:sex+sex+site_t3, data = T1)
+  lm_r.sq1[i] = summary(T1_lm_model)$r.sq
+  T2_lm_model = lm(T2[[i]]~(age)+age:sex+sex+site_t3, data = T2)
+  lm_r.sq2[i] = summary(T2_lm_model)$r.sq
+  T1_gam_model = gam(T1[[i]]~s(age)+age:sex+sex+site_t3, data = T1)
+  gam_r.sq1[i] = summary(T1_gam_model)$r.sq
+  T2_gam_model = gam(T2[[i]]~s(age)+age:sex+sex+site_t3, data = T2)
+  gam_r.sq2[i] = summary(T2_gam_model)$r.sq
+  tmp_data$fit = c(predict(T1_lm_model),predict(T1_gam_model))
+  data_TP1[[i]] = tmp_data
+  tmp_data$fit = c(predict(T2_lm_model),predict(T2_gam_model))
+  data_TP2[[i]] = tmp_data
 }
 
-# we can do the same for non-linear models
-smooth_plot = list()
+# now, we can plot the data treating it as cross-sectional data, differentiating between time points
+TP1_plot = list()
+TP2_plot = list()
 for (i in 1:length(y_lab)){
-  smooth_plot[[i]] = ggplot(data=linear_predictions[[i]],aes(x=age, y=fit,color = TP))+ 
-    geom_smooth(method = "gam") +
+  TP1_plot[[i]] = ggplot(data=data_TP1[[i]],aes(x=age, y=fit,color = model))+
+    geom_smooth(method = "lm", color= "#E69F00") +
+    geom_smooth(method = "gam", color = "#56B4E9") +
+    geom_line(aes(y=0),linetype="dotted")+
     xlab("Age")+
-    ylab (paste(y_lab[i]))+ 
-    theme_bw() + 
-    theme(panel.grid.major = element_blank(), 
+    ylab (paste(y_lab[i]))+
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())+
     theme(legend.position = "none") +
-    labs(title = paste("TP1 R2 = ",round(gam_r.sq1[i],digits = 4), "      ","TP2 R2 = ", round(gam_r.sq2[i],digits = 4)))
-}
-
-lm_plots = do.call("grid.arrange", c(lm_plot,ncol = 3))
-smooth_plots = do.call("grid.arrange", c(smooth_plot,ncol = 3))
-ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/ADJ_smooth_ageing_plots.pdf",smooth_plots, width = 15, height = 20)
-ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/ADJ_lm_ageing_plots.pdf",lm_plots, width = 15, height = 20)
-
-# SCATTER PLOT OF PREDICTED VALUES >> NOT DIFFERENTIATING BETWEEN TIME POINTS ####
-# now, plot each of the predictions in a scatter plot with two time points and a fitted line
-lm_plot = list()
-for (i in 1:length(y_lab)){
-  lm_plot[[i]] = ggplot(data=linear_predictions[[i]],aes(x=age, y=fit,color = TP))+ 
-    #geom_line(aes(y=fit,group=ID),alpha=.4,lwd=.5) + #,col=TP
-    #geom_point(aes(y=fit,shape=factor(TP), col=TP),alpha=.6) +#
-    #geom_ribbon(aes(group=TP,ymin=lwr,ymax=upr),alpha=0.3,fill="grey") + 
-    geom_smooth(method = "lm") +
-    # stat_regline_equation(
-    #   aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~")),
-    #   formula = formula(paste("data"[i]"~age")), data =data) +
-    #geom_smooth(method = "lm") +
-    #stat_regline_equation() +
-    #stat_cor()+
+    labs(title = paste("linear R2 = ",round(lm_r.sq1[i],digits = 4), "      ","non-linear R2 = ", round(gam_r.sq1[i],digits = 4)))
+  TP2_plot[[i]] = ggplot(data=data_TP2[[i]],aes(x=age, y=fit,color = model))+
+    geom_smooth(method = "lm", color= "#E69F00") +
+    geom_smooth(method = "gam", color = "#56B4E9") +
+    geom_line(aes(y=0),linetype="dotted")+
     xlab("Age")+
-    ylab (paste(y_lab[i]))+ 
-    theme_bw() + 
-    theme(panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank())+
-    theme(legend.position = "none")+
-    labs(title = paste("TP1 R2 = ",round(lm_r.sq1[i],digits = 4), "      ","TP2 R2 = ", round(lm_r.sq2[i],digits = 4)))
-}
-
-# we can do the same for non-linear models
-smooth_plot = list()
-for (i in 1:length(y_lab)){
-  smooth_plot[[i]] = ggplot(data=linear_predictions[[i]],aes(x=age, y=fit,color = TP))+ 
-    geom_smooth(method = "gam") +
-    xlab("Age")+
-    ylab (paste(y_lab[i]))+ 
-    theme_bw() + 
-    theme(panel.grid.major = element_blank(), 
+    ylab (paste(y_lab[i]))+
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())+
     theme(legend.position = "none") +
-    labs(title = paste("TP1 R2 = ",round(gam_r.sq1[i],digits = 4), "      ","TP2 R2 = ", round(gam_r.sq2[i],digits = 4)))
+    labs(title = paste("linear R2 = ",round(lm_r.sq1[i],digits = 4), "      ","non-linear R2 = ", round(gam_r.sq1[i],digits = 4)))
 }
+TP1_plots = do.call("grid.arrange", c(TP1_plot,ncol = 3))
+TP2_plots = do.call("grid.arrange", c(TP2_plot,ncol = 3))
+ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/TP1_ageing_plots.pdf",TP1_plots, width = 15, height = 20)
+ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/TP2_ageing_plots.pdf",TP2_plots, width = 15, height = 20)
 
-lm_plots = do.call("grid.arrange", c(lm_plot,ncol = 3))
-smooth_plots = do.call("grid.arrange", c(smooth_plot,ncol = 3))
-ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/ONE_LINE_ADJ_smooth_ageing_plots.pdf",smooth_plots, width = 15, height = 20)
-ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/ONE_LINE_ADJ_lm_ageing_plots.pdf",lm_plots, width = 15, height = 20)
+
+# # If interesting: plot linear and non-linear (loess) fits in one plot
+# for (i in 1:length(y_lab)){
+#   lm_plot[[i]] = sjp.poly(linear_models[[i]], "age",1, show.scatter = FALSE, show.loess = TRUE, show.loess.ci = TRUE) + theme_sjplot() + ylab(y_lab[i]) + xlab("Age") +theme(legend.position = "")
+# }
+# 
+# lm_plots = do.call("grid.arrange", c(lm_plot,ncol = 3))
+# lmplots
+# ggsave("/home/max/Documents/Projects/Diffusion/UKBlong/Results/poly_lm_ageing_plots.pdf",lm_plots, width = 15, height = 20)
